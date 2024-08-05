d2e7521c1774624557e05b36dc48e4d024d3cac4
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-08-05 11:30:04 +0300

    skeletonui init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+</script>
+
+<div
+    class="flex flex-col items-center justify-center space-y-4 max-w-screen-xl min-h-screen mx-auto overflow-auto"
+>
+    <slot></slot>
+</div>
